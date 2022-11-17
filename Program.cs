869d2c3ede884e6e8265df52385e39562865475c
0xbtc2 <118485385+0xbtc2@users.noreply.github.com>
2022-11-17 22:29:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Vols la pizza vegetariana? ");
+string input = Console.ReadLine();
+if (input == "si")
+{
+    Console.WriteLine("els ingredients vegetarians són  1. pebrot i 2. tofu.");
+    Console.WriteLine("Quin ingredient vols? ");
+    input = Console.ReadLine();
+    int ingredient = int.Parse(input);
+    if (ingredient == 1)
+    {
+        Console.WriteLine("La pizza és vegetariana i du mozzarella, tomàquet i pebrot");
+    }
+    else
+    {
+        Console.WriteLine("La pizza és vegetariana i du mozzarella, tomàquet i tofu");
+    }
+}
+else
+{
+    Console.WriteLine("els ingredients no vegetarians són 1. Peperoni, 2. Pernil i 3. Salmó.");
+    Console.WriteLine("Quin ingredient vols? ");
+    input = Console.ReadLine();
+    int ingredient = int.Parse(input);
+    if (ingredient == 1)
+    {
+        Console.WriteLine("La pizza no és vegetariana i du mozzarella, tomàquet i peperoni");
+    }
+    else if (ingredient == 2)
+    {
+        Console.WriteLine("La pizza no és vegetariana i du mozzarella, tomàquet i pernil");
+    }
+    else
+    {
+        Console.WriteLine("La pizza no és vegetariana i du mozzarella, tomàquet i salmó");
+    }
+
+}
